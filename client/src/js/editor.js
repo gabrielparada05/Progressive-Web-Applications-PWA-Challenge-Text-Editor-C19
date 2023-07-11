8a458665ba8264a567c002f63188fcb59798611d
Gabriel Parada <gabrielparada05@gmail.com>
2023-07-11 14:56:57 -0400

    trying to set db

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -24,19 +24,25 @@ export default class {
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
+    getDb()
+    .then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
+    })
+    .catch((error) => {
+      console.error('Error loading data from IndexedDB:', error);
+      this.editor.setValue(localData || header);
     });
 
-    this.editor.on('change', () => {
-      localStorage.setItem('content', this.editor.getValue());
+    this.editor.on('blur', async () => {
+      console.log('The editor has lost focus');
+      await putDb(localStorage.getItem('content'));
     });
 
     // Save the content of the editor when the editor itself is loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+			putDb(localStorage.getItem('content'));
     });
   }
 }
